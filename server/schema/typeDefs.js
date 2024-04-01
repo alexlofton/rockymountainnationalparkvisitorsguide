736a91a699c8024d95a47cf3547ad345a546f6f4
alexlofton <147463152+alexlofton@users.noreply.github.com>
2024-04-01 14:51:04 -0600

    updated and connected resolvers and typdefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -35,7 +35,7 @@ const typeDefs = gql`
     }
     
     type Mutation {
-        login(email: String, password: String):Auth
+        login(username: String, password: String):Auth
         addUser(username: String, password: String):Auth
         saveTrail(trailData: TrailInput):User
         removeTrail(trailId: ID):User
