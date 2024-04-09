bc72eadc11a47c78e3f9af69b85f2c317efc70ed
MoonstruckOccultist <justgive1234@gmail.com>
2024-04-09 14:40:13 -0600

    updated trails

--- client/src/pages/Trails.jsx
+++ client/src/pages/Trails.jsx
@@ -6,19 +6,19 @@ import TrailList from "../components/TrailList";
 import dummyTrailData from "../../dummyData/dumTrails.json";
 
 const Trails = () => {
-const trails = useQuery(ALL_TRAILS);
-const user = useQuery(QUERY_ME);
+  const trails = useQuery(ALL_TRAILS);
+  const user = useQuery(QUERY_ME);
 
-if (trails.loading) return <div>Loading...</div>;
-const trailData = trails.data?.allTrails || {};
-const userData = user.data?.me || {};
+  if (trails.loading) return <div>Loading...</div>;
+  const trailData = trails.data?.allTrails || {};
+  const userData = user.data?.me || {};
 
-return (
+  return (
     <div className="w100">
       {/* <input type="text" /> */}
-    <TrailList key={1} trails={trailData} user={userData} />
+      <TrailList key={1} trails={trailData} user={userData} />
     </div>
-);
+  );
 };
 
 export default Trails;
