060b20ea8b486460d4a4a72659aaed59a4bca479
alexlofton <147463152+alexlofton@users.noreply.github.com>
2024-04-08 20:11:21 -0600

    updated trail data

--- client/src/pages/Trails.jsx
+++ client/src/pages/Trails.jsx
@@ -1,26 +1,24 @@
 import { useQuery } from "@apollo/client";
 import { ALL_TRAILS, QUERY_ME } from "../utils/queries";
-import TrailList from '../components/TrailList'
+import TrailList from "../components/TrailList";
 
 // DELETE ONCE BACK END CONNECTED TO FRONT END
-import dummyTrailData from '../../dummyData/dumTrails.json';
+import dummyTrailData from "../../dummyData/dumTrails.json";
 
 const Trails = () => {
-    const {loading, data } = useQuery(ALL_TRAILS)
-    console.log("data", data)
-    const trailData = data?.allTrails|| {}
-    console.log(trailData)
+const trails = useQuery(ALL_TRAILS);
+const user = useQuery(QUERY_ME);
 
-    const { loading: loadingUser, data: dataUser } = useQuery(QUERY_ME)
-    const userData = dataUser?.me|| {}
-    console.log(userData)
-    //console.log(dummyTrailData)
-    return (
-        <div className="w100">
-            {/* <input type="text" /> */}
-            <TrailList key={1} trails={trailData}/>
-        </div>
-    )
+if (trails.loading) return <div>Loading...</div>;
+const trailData = trails.data?.allTrails || {};
+const userData = user.data?.me || {};
+
+return (
+    <div className="w100">
+      {/* <input type="text" /> */}
+    <TrailList key={1} trails={trailData} user={userData} />
+    </div>
+);
 };
 
 export default Trails;
