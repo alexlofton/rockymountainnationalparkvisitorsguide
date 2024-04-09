db10cdba23058c3ee44c1d619a6eeeb2582c78d1
Lillian Vanderbloemen <lillvander18@gmail.com>
2024-04-08 21:38:00 -0600

    fixed styles on wildlife and vm

--- client/src/pages/Wildlife.jsx
+++ client/src/pages/Wildlife.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Heading } from '@chakra-ui/react';
 import WildlifeComponent from '../components/Wildlife/WildlifeComponent';
+import '../App.css';
 
 
 function WildlifePage() {
@@ -10,42 +11,29 @@ function WildlifePage() {
       name: 'Black Bear',
       imageSrc:'/assets/animals/bear.jpg',
       description: 'In the majestic landscape of Rocky Mountain National Park, the black bear roams as a symbol of wilderness and resilience.  Its keen senses and powerful physique enable it to navigate the rugged slopes and dense forests with ease.',
-      seen: false
     },
     {
       id: 2,
       name: 'Moose',
       imageSrc: '/assets/animals/moose.jpg',
       description: 'Towering above the landscape with its massive frame and impressive antlers, the moose commands attention and respect. Often found grazing in meadows or wading through mountain lakes and streams, this magnificent creature is perfectly adapted to its natural habitat.',
-      seen: false
     },
 
     {
-      id: 2,
+      id: 3,
       name: 'Bighorn Sheep',
       imageSrc: '/assets/animals/bighornsheep.jpg',
       description: "The bighorn sheep commands attention with its imposing presence and rugged beauty. With its magnificent curved horns and rugged, muscular build, this iconic creature embodies a spirit of mastery in this alpine wilderness.",
-      seen: false
     },
 
     //Add more species or groups of species 
     
   ]);
 
-  const handleCheckboxChange = (id) => {
-    setWildlifeList(prevWildlifeList => {
-      return prevWildlifeList.map(wildlife => {
-        if (wildlife.id === id) {
-          return { ...wildlife, seen: !wildlife.seen };
-        }
-        return wildlife;
-      });
-    });
-  };
-
   return (
+    <div className="wildlife-styles">
     <Box>
-      <Heading size="xl" alignText="center>">Welcome to the Wildlife Page!</Heading>
+      <Heading size="xl">Welcome to the Wildlife Page!</Heading>
       {wildlifeList.map(wildlife => (
         <WildlifeComponent
           key={wildlife.id}
@@ -57,6 +45,7 @@ function WildlifePage() {
         />
       ))}
     </Box>
+    </div>
   );
 }
 
